bf33837172eca8c84aedd51636f2518d58c50737
Zero <zero.red.dev@gmail.com>
2026-01-04 20:48:10 +0000

    Remove Vercel Analytics for DNS problems in vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './reset.scss'
-import { Analytics } from '@vercel/analytics/react'
+// import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Analytics />
+    {/* <Analytics /> */}
   </React.StrictMode>,
 )
